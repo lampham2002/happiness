0f3d3cfebe743e978ff5e1ac0404a51580d69f9f
lam pham <lam.pham@nexlab.tech>
2023-03-29 16:49:36 +0700

    first commit

---
+++ main.js
@@ -0,0 +1,26 @@
+
+ScrollReveal({ 
+    reset: true,
+    distance: '300px', 
+    duration: 2000,
+    delay: 200,
+   
+    
+});
+
+ScrollReveal().reveal('.hinh,  .of, .coa, .the, .our', { origin: 'left' });
+ScrollReveal().reveal(' .slider, .simple', { delay: 700 }); 
+
+ScrollReveal().reveal('.hight, .hightttt, .where', { delay: 500 }); 
+
+ScrollReveal().reveal(' .page', { origin: 'top' }); 
+
+ScrollReveal().reveal('.img',{origin: 'bottom'})
+
+ScrollReveal().reveal('.save, .using, .all',{origin: 'top'})
+
+ScrollReveal().reveal('.text-content',{origin: 'bottom'})
+
+
+               
+
